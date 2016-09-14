2dd1500ed0268228da95a208140b7c317d973092
jneira <atreyu.bbb@gmail.com>
2016-09-14 23:21:52 +0200

    Assignment basic complete

--- assignments/1/js/app.js
+++ assignments/1/js/app.js
@@ -13,7 +13,7 @@ function MainController ($scope) {
       $scope.message = "Please enter data first";
       return;
     }
-    var items=$scope.menu.split(",")
+    var items=$scope.menu.split(",");
     if (items.length <= 3)
       $scope.message = "Enjoy!";
     else 
